b4f59045aaf2629c18c3adb8638bd81d81464776
Vardan <69721477+melvard@users.noreply.github.com>
2020-08-16 16:55:29 +0400

    SomeChanges

--- GITC_Project.py
+++ GITC_Project.py
@@ -2,6 +2,9 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
 
 df = pd.read_csv('suicide.csv')
 
@@ -16,13 +19,11 @@ dd =df.isnull()
 ss = df.groupby(['country','year']).sum()
 df.groupby(['country','year']).sum()
 
-#adding new columns for age-group and sex
-df['Age_group'] = ['A' if i=='5-14 years' else 'B' if i=='15-24 years'
-                   else 'C' if i=='25-34 years' else 'D' if i=='35-54 years' 
-                   else 'E' if i=='55-74 years' else 'F' for i in df.age]
 
-df['e.	Gender_num '] = ['0' if i =='male' else '1' for i in df.sex]
-
+#creating lists for every group for loops
+gender_list = ['female', 'male']
+ageGroup_list = ['5-14 years', '15-24 years', '25-34 years', '35-54 years',
+            '55-74 years', '75+ years']
 #creating new dataframe  grouped by country
 df_countryGroup = df.groupby(['country']).sum()
 
@@ -54,7 +55,6 @@ plt.scatter(x,y)
 plt.show()
 
 #createing regressor for predictions
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(x, y)
 
@@ -68,6 +68,8 @@ plt.show()
 
 #predit for any year
 y_pre = regressor.predict([[2025]])
+
+#create empty dataframes to add countries with suicide statistics
 statistics_withCoefficent = pd.DataFrame(columns=['Country', 'suicide_coefficient'])
 statistics_withCases = pd.DataFrame(columns = ['Country', 'Population_number', 'suicide_cases'])
 
@@ -82,6 +84,7 @@ def suicide_coefficient_counter(year, m_country_df):
     total_suicides= m_country_df[m_country_df.year == year].groupby(['age']).sum()['suicides_no'].sum()
     population = m_country_df[m_country_df.year == year].groupby(['age']).sum()['population'].sum()
     suicide_coefficient = total_suicides/population
+    
     return suicide_coefficient
     
 def suicide_cases_in_population(year, population_for_cases, m_country_df):
@@ -90,16 +93,16 @@ def suicide_cases_in_population(year, population_for_cases, m_country_df):
     k = population/population_for_cases
     print(total_suicides, population, k)
     return total_suicides/k
-    
-for country_name in Countries_df_dic:
-    country_df = Countries_df_dic[country_name]#get value of dic with key
-    missing_val_count_by_column = (country_df.isnull().sum())
+
+def predict_and_createplot_for_country(m_counrty_df,gender_list, ageGroup_list, year):
+    country_name = m_counrty_df['country'].iloc[0]
+    missing_val_count_by_column = (m_counrty_df.isnull().sum())
     #print(missing_val_count_by_column[missing_val_count_by_column > 0])
-    country_df = country_df.dropna(axis = 0)  # the lines has null values are deleted
-    country_df.head()
+    m_counrty_df = m_counrty_df.dropna(axis = 0)  # the lines has null values are deleted
+    m_counrty_df.head()
     
-    x = np.array(country_df.loc[:,'year']*1.0).reshape(-1,1)
-    y = np.array(country_df.loc[:,'suicides_no'],dtype = np.float32).reshape(-1,1)
+    x = np.array(m_counrty_df.loc[:,'year']*1.0).reshape(-1,1)
+    y = np.array(m_counrty_df.loc[:,'suicides_no'],dtype = np.float32).reshape(-1,1)
     
     #Scatter Plot
     plt.figure(figsize = [10,10])
@@ -110,13 +113,36 @@ for country_name in Countries_df_dic:
     
     reg = LinearRegression()
     predict_space = np.linspace(min(x), max(x)).reshape(-1,1)  
-    gender_list = ['female', 'male']
-    ageGroup_list = ['5-14 years', '15-24 years', '25-34 years', '35-54 years',
-            '55-74 years', '75+ years']
+    predicition_forThisCountry = 0;
+    for sex in gender_list:
+        for age_group in ageGroup_list:
+                df_withOneGender = m_counrty_df[m_counrty_df['sex'] == sex]
+                data_sex = df_withOneGender[df_withOneGender['age'] == age_group]
+                x_sex = np.array(data_sex.loc[:,'year']).reshape(-1,1)
+                y_sex = np.array(data_sex.loc[:,'suicides_no']).reshape(-1,1)
+                reg.fit(x_sex,y_sex)                                              
+                predicted = reg.predict(predict_space)
+                print(country_name,"\t| ",sex,"\t| ", age_group, '\t| R^2 Score: ', reg.score(x_sex,y_sex))                       
+                plt.plot(predict_space, predicted, color = 'black', linewidth = 2)
+                plt.scatter(x_sex,y_sex)
+                plt.title(country_name+" - " + sex + " " + age_group)
+                plt.xlabel('Year')
+                plt.ylabel('Suicides number')
+                plt.show()
+                predicition_forThisCountry += reg.predict([[year]])
+
+    print(country_name +f"---Suicides in {year} will be", predicition_forThisCountry)
+    print("\033[1;31;43m", f"y = {reg.intercept_} + {reg.coef_}X","\033[1;37;0m")
+
+    
+for country_name in Countries_df_dic:
+    country_df = Countries_df_dic[country_name]#get country dataframe of dic with key
+    predict_and_createplot_for_country(country_df, gender_list, ageGroup_list, 2021)
+    
     
     #using definition for counting k coefficient of suicide per year
-    choosen_year_for_parsing = 2014;
-    population_number = 500000;
+    choosen_year_for_parsing = 1991;
+    population_number = 100000;
     k_suicide = suicide_coefficient_counter(choosen_year_for_parsing, country_df) 
     statistics_withCoefficent = statistics_withCoefficent.append({'Country': country_name,
                                     'suicide_coefficient':k_suicide,
@@ -124,36 +150,14 @@ for country_name in Countries_df_dic:
                                    ignore_index=True)
     
     
-    #
+    #using definition for counting suicide cases in custom population 
     suicide_cases = suicide_cases_in_population(choosen_year_for_parsing, population_number, country_df)
     statistics_withCases = statistics_withCases.append({'Country': country_name,
                                     'Population_number': population_number,
                                     'suicide_cases': suicide_cases,
                                     'year':choosen_year_for_parsing},
                                    ignore_index=True)
-    predicition_forThisCountry = 0;
-    for sex in gender_list:
-        for age_group in ageGroup_list:
-            data_1 = country_df[country_df['sex'] == sex]
-            data_sex = data_1[data_1['age'] == age_group ]
-            x_sex = np.array(data_sex.loc[:,'year']).reshape(-1,1)
-            y_sex = np.array(data_sex.loc[:,'suicides_no']).reshape(-1,1)
-            reg.fit(x_sex,y_sex)                                              
-            predicted = reg.predict(predict_space)                     
-            print(country_name,"\t| ",sex,"\t| ", age_group, '\t| R^2 Score: ', reg.score(x_sex,y_sex))                       
-            plt.plot(predict_space, predicted, color = 'black', linewidth = 2)
-            plt.scatter(x_sex,y_sex)
-            plt.title(country_name+" - " + sex + " " + age_group)
-            plt.xlabel('Year')
-            plt.ylabel('Suicides number')
-            plt.show()
-            predicition_forThisCountry += reg.predict([[2018]])
-
-    
-    print(country_name +"---Suicides in 2018 will be", predicition_forThisCountry)
-    print("\033[1;31;43m", f"y = {reg.intercept_} + {reg.coef_}X","\033[1;37;0m")
-    
-
+  
 
 #Creating suicide with coefficient statistics plot 
 import math
@@ -172,8 +176,8 @@ ax.set_xlabel('Suicide coefficient')
 ax.set_title('Suicide coefficient for countries in'+" "+str(stat.year[1]))
 plt.show()
 
-#Creating suicide with cases statistics plot 
 
+#Creating suicide with cases statistics plot 
 stat = statistics_withCases
 plt.rcdefaults()
 fig, ax = plt.subplots()
@@ -190,9 +194,12 @@ i = 0
 for suicide_cases in stat.suicide_cases:
     ax.text(suicide_cases,
             i,
-            str(0 if math.isnan(suicide_cases) else int(suicide_cases)),
+            str(np.around(suicide_cases, 2) if math.isnan(suicide_cases) or( suicide_cases<1 )else int(suicide_cases)),
             fontsize = 15,
             verticalalignment = "center")
     i = i+1
 plt.show()
+
+#predict example (just change country key name of dictionary to predict and create plots)
+predict_and_createplot_for_country(Countries_df_dic['Russian'], gender_list, ageGroup_list, 2021)
 
